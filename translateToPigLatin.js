8a5e4284f12d0e1ac7237bc8d37123ba3e365700
szymon-rogus <sszymek98@gmail.com>
2020-05-01 20:05:42 +0200

    with rules version 1.1

--- translateToPigLatin.js
+++ translateToPigLatin.js
@@ -2,12 +2,10 @@ var vowels = [97, 101, 105, 111, 117, 121]
 
 $(".dropdown-menu a").click(function(){
     $("#btnGroupDrop1").html($(this).text());
-    if($("#write").text() == "English")
-        pigLatin()
+    pigLatin()
   });
 
 document.getElementById("translate").addEventListener("click", () => {
-    if($("#write").text() == "English")
         pigLatin()
 });
 
